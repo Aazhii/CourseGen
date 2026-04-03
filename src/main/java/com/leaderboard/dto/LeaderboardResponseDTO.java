61c480ff3cc3f34a56d399eca4947a4847bbbc3c
arikaran03 <arikaran258@gmail.com>
2026-04-03 23:08:54 +0530

    feat: enhance leaderboard functionality with additional user metrics and username population
    Signed-off-by: arikaran03 <arikaran258@gmail.com>

--- src/main/java/com/leaderboard/dto/LeaderboardResponseDTO.java
+++ src/main/java/com/leaderboard/dto/LeaderboardResponseDTO.java
@@ -1,9 +1,13 @@
 package com.leaderboard.dto;
 
 public class LeaderboardResponseDTO extends RankScoreDTO {
-    public LeaderboardResponseDTO(int rank, Long userId, int score) {
+    public LeaderboardResponseDTO(int rank, Long userId, int score, String username, int coursesCompleted, int currentStreak, int weeklyPoints) {
         this.rank = rank;
         this.userId = userId;
         this.score = score;
+        this.username = username;
+        this.coursesCompleted = coursesCompleted;
+        this.currentStreak = currentStreak;
+        this.weeklyPoints = weeklyPoints;
     }
 }
